7b3dd972de7c36a0793d41626e3256f04f0d605f
Phuong <ngo.phuong4296@gmail.com>
2020-10-22 13:14:24 +0300

    added bloggers journey to the project

--- src/pages/TravelBlog/index.js
+++ src/pages/TravelBlog/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './index.module.css';
+import WhyGlobuzzer from './WhyGlobuzzer/WhyGlobuzzer';
 import WhoWeAre from './WhoWeAre/WhoWeAre';
+import BloggersJourney from './BloggersJourney/BloggersJourney';
 const index = () => {
     return (
         <div className={styles.wrapper}>
+            <WhyGlobuzzer/>
             <WhoWeAre/>
+            <BloggersJourney/>
         </div>
     );
 }
