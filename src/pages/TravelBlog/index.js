dbf1d252a4c7f9baa114696f99bbd21cde1d86a5
Phuong <ngo.phuong4296@gmail.com>
2020-10-22 15:23:12 +0300

    fixed unnecessary gap between sections issue

--- src/pages/TravelBlog/index.js
+++ src/pages/TravelBlog/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import styles from './index.module.css';
 import Header from './Header/Header';
 import MainBody from '../../components/TravelBlog/body/MainBody';
 import HowItWorks from './HowItWorks/HowItWorks';
 import WhyGlobuzzer from './WhyGlobuzzer/WhyGlobuzzer';
 import WhoWeAre from './WhoWeAre/WhoWeAre';
 import BloggersJourney from './BloggersJourney/BloggersJourney';
+import {Footer} from '../../components/Footer/Footer';
 const index = () => {
     return (
         <div>
             <Header/>
-                <HowItWorks/>
+            <HowItWorks/>
                 <WhyGlobuzzer/>
                 <WhoWeAre/>
                 <BloggersJourney/>
+            <Footer/>
         </div>
     );
 }
