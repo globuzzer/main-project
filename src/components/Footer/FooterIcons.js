0639dfb34023b44262f4964bc303bce308175c54
Phuong <ngo.phuong4296@gmail.com>
2020-10-27 14:25:04 +0200

    fixed footer

--- src/components/Footer/FooterIcons.js
+++ src/components/Footer/FooterIcons.js
@@ -3,8 +3,8 @@ import {
   FaPlayCircle,
   FaLinkedinIn,
   FaInstagram,
-  FaFacebookF,
   FaTwitter,
+  FaFacebook,
   FaPinterestP,
 } from "react-icons/fa";
 import "./style.css";
@@ -13,10 +13,10 @@ const FooterIcons = () => {
     <>
       <div className="footer-content-icon-container">
         <a
-          className="footer_icons_container"
+          className="footer-icons-container"
           href="https://www.facebook.com/Globuzzer/"
         >
-          <FaFacebookF className="footer-icons" />
+          <FaFacebook className="footer-icons" />
         </a>
         <a
           className="footer-icons-container"
