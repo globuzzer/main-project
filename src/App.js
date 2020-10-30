ae022ba749788139df4052047b64eb2a9b33f7de
Phuong <ngo.phuong4296@gmail.com>
2020-10-30 11:47:50 +0200

    added sign up component

--- src/App.js
+++ src/App.js
@@ -14,13 +14,15 @@ import Helsinki from "./pages/Helsinki";
 import ScrollToTop from "./utils/ScrollToTop";
 import TravelBlog from './pages/TravelBlog/index';
 import Section from './pages/Section/index';
+import SignUpSection from './pages/Section/SignUp/SignUpSection';
+import Package from './pages/Section/SignUp/Package';
 const App = () => {
   const location = useLocation();
   const { pathname } = location;
 
   return (
     <>
-      {pathname !== "/signup" && pathname !== "/own-city-section" && pathname !== "/travel-blog" && pathname != "/section" && (
+      {pathname !== "/signup" && pathname !== "/own-city-section" && pathname !== "/travel-blog" && pathname != "/section" && pathname !="/cities" && pathname !="/cities/:city" && (
         <Navigation />
       )}
       <ScrollToTop />
@@ -58,6 +60,8 @@ const App = () => {
         <Route path='/section'>
           <Section/>
         </Route>
+        <Route exact path = '/cities'  component={SignUpSection}/>
+        <Route exact path='/cities/:city'  component={Package}/>
       </Switch>
     </>
   );
