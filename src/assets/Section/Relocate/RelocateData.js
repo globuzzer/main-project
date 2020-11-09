643c46823c7cfc1ea88224a27758a836a612d4d1
Phuong <ngo.phuong4296@gmail.com>
2020-11-09 13:53:00 +0200

    added all components of section page

---
+++ src/assets/Section/Relocate/RelocateData.js
@@ -0,0 +1,85 @@
+import FlagIcon from './flag-icon.svg';
+import author from "./author-img.jpg";
+import heart from "./like-icon.svg";
+import share from "./share-icon.svg";
+
+export const RelocateData = [
+    {
+        img:FlagIcon,
+        price: "€20",
+        title:"Relocating to Stockholm",
+        textOne:"What do I need to pack?",
+        textTwo:"How is the swedish way of life?",
+        textThree:"What visa do I require for Sweden?",
+        author: author,
+        name:"Sofia",
+        heart: heart,
+        number:"15k",
+        share: share
+    },
+    {
+        img:FlagIcon,
+        price: "€20",
+        title:"Relocating to Stockholm",
+        textOne:"What do I need to pack?",
+        textTwo:"How is the swedish way of life?",
+        textThree:"What visa do I require for Sweden?",
+        author: author,
+        name:"Sofia",
+        heart: heart,
+        number:"15k",
+        share: share
+    },
+    {
+        img:FlagIcon,
+        price: "€20",
+        title:"Relocating to Stockholm",
+        textOne:"What do I need to pack?",
+        textTwo:"How is the swedish way of life?",
+        textThree:"What visa do I require for Sweden?",
+        author: author,
+        name:"Sofia",
+        heart: heart,
+        number:"15k",
+        share: share
+    },
+    {
+        img:FlagIcon,
+        price: "€20",
+        title:"Relocating to Stockholm",
+        textOne:"What do I need to pack?",
+        textTwo:"How is the swedish way of life?",
+        textThree:"What visa do I require for Sweden?",
+        author: author,
+        name:"Sofia",
+        heart: heart,
+        number:"15k",
+        share: share
+    },
+    {
+        img:FlagIcon,
+        price: "€20",
+        title:"Relocating to Stockholm",
+        textOne:"What do I need to pack?",
+        textTwo:"How is the swedish way of life?",
+        textThree:"What visa do I require for Sweden?",
+        author: author,
+        name:"Sofia",
+        heart: heart,
+        number:"15k",
+        share: share
+    },
+    {
+        img:FlagIcon,
+        price: "€20",
+        title:"Relocating to Stockholm",
+        textOne:"What do I need to pack?",
+        textTwo:"How is the swedish way of life?",
+        textThree:"What visa do I require for Sweden?",
+        author: author,
+        name:"Sofia",
+        heart: heart,
+        number:"15k",
+        share: share
+    }
+]
